b90af353589453af03b8fdc000a63803c2759f8d
KRA <KRA@KRA-PC>
2019-05-04 12:20:33 +0400

    Немного отрисовал стол

--- NormaMeasureDevices/SAC/TableMap.Designer.cs
+++ NormaMeasureDevices/SAC/TableMap.Designer.cs
@@ -34,7 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(515, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "TableMap";
             this.Text = "TableMap";
             this.ResumeLayout(false);
--- NormaMeasureDevices/SAC/TableMap.cs
+++ NormaMeasureDevices/SAC/TableMap.cs
@@ -13,8 +13,11 @@ namespace NormaMeasure.Devices.SAC
 {
     public partial class TableMap : Form
     {
-        int ModulWidth = 40;
-        int ModulHeight = 120;
+        int ModulsAmount = 26;
+
+        int ModulRightPadding = 4;
+        int ModulWidth = 60;
+        int ModulHeight = 200;
         public TableMap()
         {
             InitializeComponent(); 
@@ -25,32 +28,60 @@ namespace NormaMeasure.Devices.SAC
 
         private void DrawCommUnits()
         {
-            for(int i = 0; i<26; i++)
+            for(int i = 0; i< ModulsAmount; i++)
             {
-                InitPanel(i);
+                Panel p = InitPanel(i);
             }
+            this.Size = new Size((ModulsAmount/2)*(ModulWidth + ModulRightPadding+3), ModulHeight*2+35+60);
         }
 
-        private void InitPanel(int idx)
+        private Panel InitPanel(int idx)
         {
             Panel p = new Panel();
             p.Name = $"moudule_{idx}";
             p.Parent = this;
-            p.Paint += new PaintEventHandler(TableMap_Paint1);
-            bool isDK = idx > 12;
-            int xPos = isDK ? (idx - 13) * (ModulWidth + 4) : idx * (ModulWidth+4);
+
+            bool isDK = idx > ModulsAmount/2 - 1;
+            int xPos = isDK ? (idx - 13) * (ModulWidth + ModulRightPadding) : idx * (ModulWidth+ ModulRightPadding);
             int yPos = isDK ? 0 : ModulHeight + 30;
             xPos += 10;
             yPos += 10;
-            Debug.WriteLine($"InitPanel_{ idx } xPos = {xPos} yPos = {yPos}");
+            //Debug.WriteLine($"InitPanel_{ idx } xPos = {xPos} yPos = {yPos}");
             p.Size = new Size(ModulWidth, ModulHeight);
-            p.Location = new Point(xPos, yPos);
-            p.BackColor = Color.Aquamarine;
 
+            p.BackColor = Color.FromArgb(216, 232, 255);
             p.Click += P_Click;
+            p.Location = new Point(xPos, yPos);
+            p.Paint += new PaintEventHandler(TableMap_Paint1);
+            p.Refresh();
+            InitPairsOnModule(p, idx);
+            return p;
+
             //p.Visible = true;
             //p.BackColor = Color.Aquamarine;
 
+        }
+
+        protected void InitPairsOnModule(Panel p, int moduleId)
+        {
+            for(int i = 0; i<4; i++)
+            {
+                InitPair(p, i);
+            }
+        }
+
+        protected void InitPair(Panel p, int pairIdx)
+        {
+            int pairHeight = 30;
+            int marginY = (p.Height - pairHeight*4)/5;
+            int locY = (pairHeight+marginY)*pairIdx+marginY;
+            int locX = 5;
+            Panel pairPanel = new Panel();
+            pairPanel.Parent = p;
+            pairPanel.Width = p.Width - 10;
+            pairPanel.Height = pairHeight;
+            pairPanel.Location = new Point(locX, locY);
+            pairPanel.BackColor = Color.Black;
         }
 
         private void P_Click(object sender, EventArgs e)
@@ -62,8 +93,9 @@ namespace NormaMeasure.Devices.SAC
         {
             System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
             System.Drawing.Graphics formGraphics;
-            formGraphics = this.CreateGraphics();
-            formGraphics.DrawRectangle(myPen, new Rectangle(0, 0, (sender as Panel).Width, (sender as Panel).Height));
+            formGraphics = (sender as Panel).CreateGraphics();
+            formGraphics.DrawRectangle(myPen, new Rectangle( 0, 0, (sender as Panel).Width - 2, (sender as Panel).Height - 2));
+            //formGraphics.DrawRectangle(myPen, new Rectangle((sender as Panel).Location.X-1, (sender as Panel).Location.Y-1, (sender as Panel).Width+2, (sender as Panel).Height+2));
             myPen.Dispose();
             formGraphics.Dispose();
         }
