b864e61dac4bb491313471bd0851dafd816830dd
KRA <KRA@KRA-PC>
2019-05-21 23:10:39 +0400

    Добавлен проект формирования протокола

---
+++ AppTest/WordProtocolTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NormaMeasure.ProtocolBuilder.MSWord;
+namespace AppTest
+{
+    class WordProtocolTest
+    {
+        public static void Start()
+        {
+            MSWordProtocol protocol = new MSWordProtocol();
+            protocol.Init();
+            protocol.Finalise();
+        }
+    }
+}
