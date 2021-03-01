8d29abb79ab381154ce77a3792a1fbefb6be392f
KRA <KRA@KRA-PC>
2021-03-01 19:46:02 +0400

    AddTables

---
+++ NormaDBControl/DBControl/NormaMeasure/DBEntities/DBSACBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NormaMeasure.DBControl.SAC.DBEntities
+{
+    public abstract class DBNormaBase : DBEntityBase
+    {
+        static DBNormaBase()
+        {
+            //dbName = "db_sak";
+        }
+
+    }
+}
---
+++ NormaDBControl/DBControl/NormaMeasure/DBEntities/QADocument.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace NormaMeasure.DBControl.SAC.DBEntities
+{
+    public class QADocumentNorma : DBSACBase
+    {
+        private static QADocumentNorma qaDoc;
+        private string _short_name, _full_name;
+
+        public string ShortName
+        {
+            set
+            {
+                _short_name = value;
+            }get
+            {
+                return _short_name;
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return _full_name;
+            }
+            set
+            {
+                _full_name = value;
+            }
+        }
+
+
+        #region Конструкторы
+
+        static QADocumentNorma()
+        {
+            qaDoc = new QADocumentNorma();
+        }
+
+        public QADocumentNorma()
+        {
+            initEntity();
+        }
+
+        public QADocumentNorma(uint id) : this()
+        {
+            _id = id;
+            GetById();
+        }
+
+
+
+        public static QADocument[] GetAll()
+        {
+            QADocument[] els = new QADocument[] { };
+            DataTable dt = qaDoc.GetAllFromDB();
+            if (dt.Rows.Count > 0)
+            {
+                els = new QADocument[dt.Rows.Count];
+                for (int i = 0; i < dt.Rows.Count; i++) els[i] = new QADocument(dt.Rows[i]);
+            }
+            return els;
+        }
+
+
+        public QADocumentNorma(DataRow row) : this()
+        {
+            FillFromDataRow(row);
+        }
+
+        #endregion 
+        protected override string getPropertyValueByColumnName(string colName)
+        {
+            string value = null;
+            switch (colName)
+            {
+                case "short_name":
+                    return $"'{ShortName}'";
+                case "full_name":
+                    return $"'{FullName}'";
+                default:
+                    return value;
+            }
+        }
+
+
+        protected override bool setPropertyByColumnName(object value, string colName)
+        {
+            switch (colName)
+            {
+                case "short_name":
+                    _short_name = value.ToString();
+                    return true;
+                case "full_name":
+                    _full_name = value.ToString();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        protected override void setDefaultProperties()
+        {
+            _full_name = "";
+            _short_name = "";
+        }
+
+        protected override void initEntity()
+        {
+           // _dbTable = DBSACTablesMigration.DocumentsTable;
+        }
+    }
+}
