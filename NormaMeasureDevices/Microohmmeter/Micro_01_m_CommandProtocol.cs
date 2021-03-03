d154587a2b0f2775d7ec47c4a1d96453fe16b884
KRA <KRA@KRA-PC>
2021-03-03 19:28:43 +0400

    Отладка измерений с микроомметра

---
+++ NormaMeasureDevices/Microohmmeter/Micro_01_m_CommandProtocol.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NormaMeasure.Devices.Microohmmeter
+{
+    class Micro_01_m_CommandProtocol : DeviceCommandProtocol
+    {
+        ushort CableLengthAddr;
+        ushort BetweenMeasureDelayAddr;
+        ushort BeforeMeasureDelayAddr;
+        ushort MaterialIdAddr;
+        ushort TemperatureAddr;
+        ushort CurrentMeasureRangeAddr;
+        ushort ConvertedResistanceValueAddr;
+        ushort ConvertedByMeasureModeResistanceValueAddr;
+        ushort MeasureModeAddr;
+        ushort StatMeasureAmountAddr;
+        ushort LengthBringingTypeIdAddr;
+        ushort CableLengthMeasureIdAddr;
+        ushort MeasureCyclesCounterAddr;
+        ushort WaitResultFlagAddr;
+        ushort LeadDiameterAddr;
+
+        protected override void InitAddressMap()
+        {
+            base.InitAddressMap();
+            PCModeFlagAddr = 0x0080;
+            MeasureLineNumberAddr = 0x0081;
+            MeasureStartFlagAddr = 0x0082;
+            MeasureStatusAddr = 0x0083;
+
+            CurrentMeasureRangeAddr = 0x0047;
+            ConvertedResistanceValueAddr = 0x0088;
+            ConvertedByMeasureModeResistanceValueAddr = 0x008A;
+            MeasureCyclesCounterAddr = 0x0096;
+            LeadDiameterAddr = 0x0042;
+            MeasureModeAddr = 0x0030;
+            CableLengthAddr = 0x0031;
+            TemperatureAddr = 0x0032;
+            BetweenMeasureDelayAddr = 0x0049;
+            BeforeMeasureDelayAddr = 0x0045;
+            StatMeasureAmountAddr = 0x0036;
+            MaterialIdAddr = 0x003A;
+            CableLengthMeasureIdAddr = 0x003C;
+            LengthBringingTypeIdAddr = 0x003F;
+
+        }
+
+
+
+    }
+}
--- NormaMeasureDevices/Microohmmeter/Microohmmeter_uOmM_01m.cs
+++ NormaMeasureDevices/Microohmmeter/Microohmmeter_uOmM_01m.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using NormaMeasure.Devices.XmlObjects;
 
 namespace NormaMeasure.Devices.Microohmmeter
 {
     public class Microohmmeter_uOmM_01m : DeviceBase
     {
+        private uint cableLength = 1000;
+        private uint beforeMeasureDelay = 200; // в миллисекундах
+        private uint betweenMeasureDelay = 1; // в секундах
+        private uint statMeasureTimes = 0;
+        private uint measureRange;
+
+        protected uint MeasureCyclesCounter = 0;
+        protected bool resultWaitFlag;
+
+        private MICRO_MEASURE_MODE measureMode = MICRO_MEASURE_MODE.RL;
         public Microohmmeter_uOmM_01m(DeviceInfo info) : base(info)
         {
             type_id = DeviceType.Microohmmeter;
@@ -16,11 +27,100 @@ namespace NormaMeasure.Devices.Microohmmeter
             type_name_full = "Микроомметр µОмМ-01м";
         }
 
+        protected override void SetMeasureParametersFromMeasureXMLState(MeasureXMLState measure_state)
+        {
+            cableLength = measure_state.MeasuredCableLength;
+            beforeMeasureDelay = measure_state.BeforeMeasureDelay;
+            betweenMeasureDelay = measure_state.AfterMeasureDelay;
+            statMeasureTimes = measure_state.AveragingTimes;
+            
+        }
+
+        protected override void InitMeasureCycleOnDevice()
+        {
+            SendMeasureParamsToDevice();
+            CommandProtocol.MeasureStartFlag = true;
+            Thread.Sleep(200);
+            IsOnMeasureCycle = CommandProtocol.MeasureStartFlag;
+            resultWaitFlag = false;
+            MeasureCyclesCounter = 0;
+        }
+
+        protected override void SendMeasureParamsToDevice()
+        {
+            base.SendMeasureParamsToDevice();
+        }
+
         public override DeviceXMLState GetXMLState()
         {
             return new DeviceXMLState(this);
         }
 
+        protected override void PCModeMeasureThreadFunction()
+        {
+            uint cyclesCounterWas = MeasureCyclesCounter;
+            DeviceWorkStatus work_status_was;
+             p = CommandProtocol as TOhmM_01_v1_CommandProtocol;
+            TeraMeasureResultStruct result;
 
+            work_status_was = WorkStatus;
+            WorkStatus = (DeviceWorkStatus)p.WorkStatus;
+
+            if (WorkStatus == DeviceWorkStatus.MEASURE)
+            {
+                if (!integratorIsStart)
+                {
+                    StartIntegrator();
+                    integratorIsStart = true;
+                }
+                else
+                {
+                    //Thread.Sleep(60);
+                    if (!p.StartIntegratorFlag)
+                    {
+
+                        result = p.MeasureResult;
+                        RawResult = (result.ConvertedValue > 0.00001) ? result.ConvertedValue * 1000.0 : 0; //Перевод в МОм
+                        ConvertedResult = (result.ConvertedByModeValue > 0.00001) ? result.ConvertedByModeValue * 1000.0 : 0; //Перевод в МОм
+                        MeasureStatusId = result.MeasureStatus;
+                        OnGetMeasureResult?.Invoke(this, new MeasureResultEventArgs(result));
+                        if (MeasureStatusId == (uint)DeviceMeasureResultStatus.SUCCESS)
+                        {
+                            integratorIsStart = false;
+                            Debug.WriteLine($"COUNTER {cyclesCounterWas} -------------------");
+                        }
+                        else
+                        {
+                            measure_cycle_flag = false;
+                            p.MeasureStartFlag = false;
+                        }
+                    }
+                    else
+                    {
+                        MeasureCyclesCounter = p.MeasureCyclesCounter;
+                        if (!p.PCModeFlag || p.MeasureLineNumber != ClientId)
+                        {
+                            measure_cycle_flag = false;
+                            p.MeasureStartFlag = false;
+                        }
+                    }
+                }
+            }
+            else if (WorkStatus == DeviceWorkStatus.IDLE || work_status_was == DeviceWorkStatus.DEPOLARIZATION)
+            {
+                measure_cycle_flag = false;
+                p.MeasureStartFlag = false;
+                //IsOnMeasureCycle = false;
+                //threadIsActive = false;
+                //break;
+            }
+        }
+    }
+
+    enum MICRO_MEASURE_MODE : ushort
+    {
+        R = 0,
+        RL = 2,
+        p = 3
     }
 }
