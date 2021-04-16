f194463edc37c5fac6c123ba1223a3984975184f
KRA <KRA@KRA-PC>
2021-04-16 13:07:00 +0400

    Добавлена блокировка второго запуска

--- TeraMicroMeasure/Program.cs
+++ TeraMicroMeasure/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +10,20 @@ namespace TeraMicroMeasure
 {
     static class Program
     {
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Process[] procs = Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName);
+            if (procs.Length > 1)
+            {
+                SetForegroundWindow(procs[0].MainWindowHandle);
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
   
